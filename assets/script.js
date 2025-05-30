99221bf78d6f0af38eeac32fe42af4931538811c
djbrown14 <djbrown14@hotmail.com>
2025-05-30 02:16:01 +0100

    Changes to css

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,17 @@
- const game = document.getElementById('.memorygame');
- const totalPairs = 6;
- const images = [];
+  const cards = document.querySelectorAll('.memory-card');
 
-    for (let i = 1; i <= totalPairs; i++) {
-        images.push(`img/img-${i}.webp`);
-    }
-
-  let cards = [...images, ...images];
-  let firstCard = null;
-  let secondCard = null;
+  let hasFlippedCard = false;
   let lockBoard = false;
-  
-  function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-        }
-  }
-
-  function createCards(imgScr) {
-    const card = document.createElement('div');
-    card.classList.add('card');
-   
-    const inner = document.createElement('div');
-    inner.classList.add('card-inner');
-
-    const frontFace = document.createElement('img');
-    frontFace.classList.add('front-face');
-
-    const backFace = document.createElement('img');
-    backFace.classList.add('back-face');
-    backFace.style.backgroundImage = "url('img/background1.webp')";
-
-    inner.appendChild(frontFace);
-    inner.appendChild(backFace);
-    card.appendChild(inner);
-
-    card.dataset.framework = imgScr;
-    card.addEventListener('click', flipCard);
-
-    return card;
-  }
-
-  function initGame() {
-    BodyReadable.innerHTML = '';
-    shuffle(cards);
-
-    cards.forEach(img => {
-        const cardElement = createCards(img);
-        board.appendChild(cardElement);
-  });
-}
+  let firstCard, secondCard;
 
   function flipCard() {
-    if (lockBoard || this.classList.contains('matched') || this === firstCard return;
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
     this.classList.add('flip');
 
-    if (!firstCard) {
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
       firstCard = this;
       return;
     }
@@ -69,21 +23,36 @@
   }
 
   function checkForMatch() {
-    const isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+  }
 
-    if (isMatch) {
-        firstCard.classList.add('matched');
-        secondCard.classList.add('matched');
-        resetBoard();
-    } else {
-        setTimeout(() => {
-      firstCard.classList.remove('flipped');
-      secondCard.classList.remove('flipped');
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+  }
+
+  function unflipCards() {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
       resetBoard();
-    }, 1000);
-    }
-}
+    }, 1500);
+  }
 
   function resetBoard() {
-    [firstCard, secondCard, lockBoard] = [null, null, false];
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
   }
+
+ (function shuffle() {
+   cards.forEach(card => {
+     let ramdomPos = Math.floor(Math.random() * 12);
+     card.style.order = ramdomPos;
+   });
+ })();
+
+  cards.forEach(card => card.addEventListener('click', flipCard));
