22126aa6b507901c0c69dd772c893a3b75f5759e
djbrown14 <djbrown14@hotmail.com>
2025-06-03 21:28:36 +0100

    Changed images in javascript

--- assets/game.js
+++ assets/game.js
@@ -1,116 +1,138 @@
-document.addEventListener('DOMContentLoaded', () => {
-  //list all card options
-  const cardArray = [
-    {
-      name: 'astro',
-      img: 'images/astro.webp'
-    },
-    {
-      name: 'baymax',
-      img: 'images/baymax.webp'
-    },
-    {
-      name: 'bumblebee',
-      img: 'images/bumblebee.webp'
-    },
-    {
-      name: 'r2d2',
-      img: 'images/r2d2.webp'
-    },
-    {
-      name: 'wall-e',
-      img: 'images/wall-e.webp'
-    },
-    {
-      name: 'wild-robot',
-      img: 'images/wild-robot.webp'
-    },
-    {
-      name: 'astro',
-      img: 'images/astro.webp'
-    },
-    {
-      name: 'baymax',
-      img: 'images/baymax.webp'
-    },
-    {
-      name: 'bumblebee',
-      img: 'images/bumblebee.webp'
-    },
-    {
-      name: 'r2d2',
-      img: 'images/r2d2.webp'
-    },
-    {
-      name: 'wall-e',
-      img: 'images/wall-e.webp'
-    },
-    {
-      name: 'wild-robot',
-      img: 'images/wild-robot.webp'
-    }
-  ]
+const cards = 
+    document.getElementsByClassName('card');
+let allImages = document.getElementsByClassName('front-face');
+let movesDisplay = document.querySelector('.moves');
+let toggledCardsArray = [];
+let move = 0;
+let winCount = 0;
+const restart = document.getElementById('restart');
 
-  cardArray.sort(() => 0.5 - Math.random())
-
-  const grid = document.querySelector('.grid')
-  const resultDisplay = document.querySelector('#result')
-  let cardsChosen = []
-  let cardsChosenId = []
-  let cardsWon = []
-
-  //create your board
-  function createBoard() {
-    for (let i = 0; i < cardArray.length; i++) {
-      const card = document.createElement('img')
-      card.setAttribute('src', 'images/blank.png')
-      card.setAttribute('data-id', i)
-      card.addEventListener('click', flipCard)
-      grid.appendChild(card)
+const imagesLinkArray = [
+    {
+        id: 1,
+        image: 
+'astro.webp',
+        newAlt: 'Astro Image'
+    },
+    {
+        id: 2,
+        image: 
+'baymax.webp',
+        newAlt: 'Baymax Image'
+    },
+    {
+        id: 3,
+        image: 
+'wild-robot.webp',
+        newAlt: 'Wild-robot Image'
+    },
+    {
+        id: 4,
+        image: 
+'bumblebee.webp',
+        newAlt: 'Bumblebee Image'
+    },
+    {
+        id: 5,
+        image: 
+'r2d2.webp',
+        newAlt: 'R2d2 Image'
+    },
+    {
+        id: 6,
+        image: 
+'wall-e.webp',
+        newAlt: 'Wall-e Image'
+    },
+    {
+        id: 7,
+        image: 
+'astro.webp',
+        newAlt: 'Astro Image'
+    },
+    {
+        id: 8,
+        image: 
+'baymax.webp',
+        newAlt: 'Baymax Image'
+    },
+    {
+        id: 9,
+        image: 
+'wild-robot.webp',
+        newAlt: 'Wild-robot Image'
+    },
+    {
+        id: 10,
+        image: 
+'bumblebee.webp',
+        newAlt: 'Bumblebee Image'
+    },
+    {
+        id: 11,
+        image: 
+'r2d2.webp',
+        newAlt: 'R2d2 Image'
+    },
+    {
+        id: 12,
+        image: 
+'wall-e.webp',
+        newAlt: 'Wall-e Image'
     }
-  }
+]
 
-  //check for matches
-  function checkForMatch() {
-    const cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
-    
-    if(optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', 'images/blank.png')
-      cards[optionTwoId].setAttribute('src', 'images/blank.png')
-      alert('You have clicked the same image!')
-    }
-    else if (cardsChosen[0] === cardsChosen[1]) {
-      alert('You found a match')
-      cards[optionOneId].setAttribute('src', 'images/white.png')
-      cards[optionTwoId].setAttribute('src', 'images/white.png')
-      cards[optionOneId].removeEventListener('click', flipCard)
-      cards[optionTwoId].removeEventListener('click', flipCard)
-      cardsWon.push(cardsChosen)
-    } else {
-      cards[optionOneId].setAttribute('src', 'images/blank.png')
-      cards[optionTwoId].setAttribute('src', 'images/blank.png')
-      alert('Sorry, try again')
-    }
-    cardsChosen = []
-    cardsChosenId = []
-    resultDisplay.textContent = cardsWon.length
-    if  (cardsWon.length === cardArray.length/2) {
-      resultDisplay.textContent = 'Congratulations! You found them all!'
-    }
-  }
+// function to restart the game
+const restartGame = () => {
+    let toggledCard = 
+        document.getElementsByClassName('card toggled');
+    imagesLinkArray.sort(() => Math.random() - 0.5);
+    Object.values(toggledCard).forEach(function (el) {
+        setTimeout(() => {
+            el.classList.remove("toggled");
+        }, 0);
+    })
+    toggledCardsArray.length = 0;
+    move = 0;
+    winCount=0;
+    movesDisplay.innerText = `Moves: ${move}`;
+    let allImagesSrc = document.getElementsByClassName('front-face');
+    Object.values(allImagesSrc).forEach((el, index)=>{
+        el.src = imagesLinkArray[index].image;
+        el.alt = imagesLinkArray[index].newAlt;
+        el.id = imagesLinkArray[index].id
+    }) 
+}
+restart.addEventListener('click', restartGame);
 
-  //flip your card
-  function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChosen.push(cardArray[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length ===2) {
-      setTimeout(checkForMatch, 500)
-    }
-  }
-
-  createBoard()
-})
+//checking for the last clicked and current 
+//clicked cards and applying changes accordingly
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function () {
+        this.classList.add("toggled");
+        toggledCardsArray.push(this);
+        let thisImgSrc = this.querySelector('.front-face').src;
+        let previousImgSrc = 
+        toggledCardsArray[toggledCardsArray.length - 2].querySelector('.front-face').src;
+        if(thisImgSrc !== previousImgSrc) {
+            toggledCardsArray.forEach(function (el) {
+                setTimeout(() => {
+                    el.classList.remove("toggled");
+                }, 500);
+            })
+            toggledCardsArray.length = 0;
+            move++;
+        }
+        else{
+            toggledCardsArray.length = 0;
+            move++;
+            winCount++;
+        }
+        movesDisplay.innerText = `Moves: ${move}`;
+        if(winCount===6){
+            setTimeout(()=>{
+                alert(`Congratulations!!! You won the game in ${move} moves.`)
+            }, 300)
+        }
+    })
+}
