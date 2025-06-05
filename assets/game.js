bb680e6d0b6d3360fb7e4d384d96fefaad63395e
djbrown14 <djbrown14@hotmail.com>
2025-06-05 18:37:28 +0100

    Trying to reset the cards

--- assets/game.js
+++ assets/game.js
@@ -4,7 +4,6 @@ let allImages = document.getElementsByClassName('front-face');
 let movesDisplay = document.querySelector('.moves');
 let toggledCardsArray = [];
 let move = 0;
-let winCount = 0;
 const restart = document.getElementById('restart');
 
 const imagesLinkArray = [
@@ -82,19 +81,17 @@ const imagesLinkArray = [
     }
 ]
 
-// function to restart the game
-const restartGame = () => {
-    let toggledCard = 
-        document.getElementsByClassName('card toggled');
-    imagesLinkArray.sort(() => Math.random() - 0.5);
-    Object.values(toggledCard).forEach(function (el) {
-        setTimeout(() => {
-            el.classList.remove("toggled");
-        }, 0);
-    })
+// function to reset the game
+function restart () {
+  resetBoard();
+  shuffleCards();
+  moves = 0
+  document.querySelector('.moves').textContent = moves;
+  cards.innerHTML = "";
+  generateCards();
+}
     toggledCardsArray.length = 0;
     move = 0;
-    winCount=0;
     movesDisplay.innerText = `Moves: ${move}`;
     let allImagesSrc = document.getElementsByClassName('front-face');
     Object.values(allImagesSrc).forEach((el, index)=>{
@@ -102,37 +99,30 @@ const restartGame = () => {
         el.alt = imagesLinkArray[index].newAlt;
         el.id = imagesLinkArray[index].id
     }) 
-}
-restart.addEventListener('click', restartGame);
+
+reset.addEventListener('click', resetGame);
 
 //checking for the last clicked and current 
 //clicked cards and applying changes accordingly
 for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function () {
-        this.classList.add("toggled");
-        toggledCardsArray.push(this);
-        let thisImgSrc = this.querySelector('.front-face').src;
-        let previousImgSrc = 
-        toggledCardsArray[toggledCardsArray.length - 2].querySelector('.front-face').src;
-        if(thisImgSrc !== previousImgSrc) {
-            toggledCardsArray.forEach(function (el) {
-                setTimeout(() => {
-                    el.classList.remove("toggled");
-                }, 500);
-            })
-            toggledCardsArray.length = 0;
-            move++;
-        }
-        else{
-            toggledCardsArray.length = 0;
-            move++;
-            winCount++;
-        }
-        movesDisplay.innerText = `Moves: ${move}`;
-        if(winCount===6){
-            setTimeout(()=>{
-                message(`Congratulations!!! You won the game in ${move} moves.`)
-            }, 300)
-        }
-    })
+  cards[i].addEventListener('click', function () {
+    this.classList.add("toggled");
+    toggledCardsArray.push(this);
+    let thisImgSrc = this.querySelector('.front-face').src;
+    let previousImgSrc = 
+    toggledCardsArray[toggledCardsArray.length - 2].querySelector('.front-face').src;
+    if(thisImgSrc !== previousImgSrc) {
+      toggledCardsArray.forEach(function (el) {
+        setTimeout(() => {
+          el.classList.remove("toggled");
+        }, 500);
+      })
+        toggledCardsArray.length = 0;
+          move++;
+    }
+    movesDisplay.innerText = `Moves: ${move}`;
+      setTimeout(()=>{
+        message(`Congratulations!!! You won the game in ${move}.`)
+      }, 300)
+  })
 }
