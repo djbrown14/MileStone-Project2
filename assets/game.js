23b65dcb8fcf724ae333a2b57f98af597477f3bd
djbrown14 <djbrown14@hotmail.com>
2025-06-09 12:17:55 +0100

    Adding cards

--- assets/game.js
+++ assets/game.js
@@ -1,125 +1,65 @@
-const cards = document.getElementsByClassName('card');
-let allImages = document.getElementsByClassName('front-face');
-let movesDisplay = document.querySelector('.moves');
-let toggledCardsArray = [];
-let move = 0;
-let winCount = 0;
-const restart = document.getElementById('restart');
-
-const imagesLinkArray = [
-    {
-        image: 'astro.webp',
-        newAlt: 'Astro Image'
-    },
-    {
-        image: 'baymax.webp',
-        newAlt: 'Baymax Image'
-    },
-    {
-        image: 'wild-robot.webp',
-        newAlt: 'Wild-robot Image'
-    },
-    {
-        image: 'bumblebee.webp',
-        newAlt: 'Bumblebee Image'
-    },
-    {
-        image: 'r2d2.webp',
-        newAlt: 'R2d2 Image'
-    },
-    {
-        image: 'wall-e.webp',
-        newAlt: 'Wall-e Image'
-    },
-    {
-        image: 'astro.webp',
-        newAlt: 'Astro Image'
-    },
-    {
-        image: 'baymax.webp',
-        newAlt: 'Baymax Image'
-    },
-    {
-        image: 'wild-robot.webp',
-        newAlt: 'Wild-robot Image'
-    },
-    {
-        image: 'bumblebee.webp',
-        newAlt: 'Bumblebee Image'
-    },
-    {
-        image: 'r2d2.webp',
-        newAlt: 'R2d2 Image'
-    },
-    {
-        image: 'wall-e.webp',
-        newAlt: 'Wall-e Image'
-    }
+let moves = 0;
+let cardList = [
+    "astro",
+    "baymax",
+    "bumblebee",
+    "r2d2",
+    "wall-e",
+    "wild-robot",
 ]
 
-// function to shuffle the images
-function shuffle(array) {
-    let currentIndex = array.length;
+let cardSet;
+let board = [];
+let rows = 4;
+let columns = 3;
 
-    // While there remain elements to shuffle...
-  while (currentIndex != 0) {
+window.onload = function() {
+    shuffleCards();
+    startGame();
+}
 
-    // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+function shuffleCards() {
+    cardSet = cardList.concat(cardList); //two of each card
+    console.log(cardSet);
+    //shuffle
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); // get random cards
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
     }
+    console.log(cardSet);
 }
 
-// function to restart the game
-const restartGame = () => {
-    let toggledCard = document.getElementsByClassName('card toggled');
-    toggledCard.forEach(toggled => {
-        toggled.remove();
-    })
-    toggledCardsArray.length = 0;
-    move = 0;
-    winCount = 0;
-    movesDisplay.innerText = `Moves: ${move}`;
-    let allImagesSrc = document.getElementsByClassName('front-face');
-    Object.values(allImagesSrc).forEach((el, index) => {
-        el.src = imagesLinkArray[index].image;
-        el.alt = imagesLinkArray[index].newAlt;
-    })
+function startGame() {
+    //arrange the board 4x3
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let cardImg = cardSet.pop();
+            row.push(cardImg); //JS
+
+            // <img id="0-0" class="card" src="astro.webp">
+            let card = document.createElement("img");
+            card.id = r.toString() + "-" + c.toString();
+            card.src = cardImg + ".webp";
+            card.classList.add("card");
+            document.getElementById("board").append(card);
+
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+
 }
 
-restart.addEventListener('click', restartGame);
-
-//checking for the last clicked and current 
-//clicked cards and applying changes accordingly
-for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener('click', function () {
-        this.classList.add("toggled");
-        toggledCardsArray.push(this);
-        const thisImgSrc = this.querySelector('.front-face').src;
-        const previousImgSrc = toggledCardsArray[toggledCardsArray.length - 2].querySelector('.front-face').src;
-        if (thisImgSrc !== previousImgSrc) {
-            toggledCardsArray.forEach(function (el) {
-                setTimeout(() => {
-                    el.classList.remove("toggled");
-                }, 500);
-            })
-            toggledCardsArray.length = 0;
-            move++;
+function hideCards() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let card = document.getElementById(r.toString() + "-" + c.toString());
+            card.src = "background1.webp";
         }
-        else {
-            toggledCardsArray.length = 0;
-            move++;
-            winCount++;
-        }
-        movesDisplay.innerText = `Moves: ${move}`;
-        if (winCount === 6) {
-            setTimeout(() => {
-                alert(`Congratulations!!! You won the game in ${move} moves.`)
-            }, 300)
-        }
-    })
+    }
 }
