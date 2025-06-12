cdbd402ebdede59a8de88379a96042dd72c0d1b2
djbrown14 <djbrown14@hotmail.com>
2025-06-12 16:33:03 +0100

    Added changes

--- assets/game.js
+++ assets/game.js
@@ -1,58 +1,157 @@
-  const cards = document.querySelectorAll('.memory-card');
+const cards = document.getElementsByClassName('memory-card');
+let allImages = document.getElementsByClassName('front-face');
+let movesDisplay = document.querySelector('.move-counter');
+let toggledCardsArray = [];
+let move = 0;
+const restart = document.getElementById('restart');
 
-  let hasFlippedCard = false;
-  let lockBoard = false;
-  let [firstCard, ], secondCard;
+const imagesLinkArray = [
+    {
+        id: 1,
+        image: 'astro.webp',
+        newAlt: 'Astro'
+    },
+    {
+        id: 2,
+        image: 'atom.webp',
+        newAlt: 'Atom'
+    },
+    {
+        id: 3,
+        image: 'baymax.webp',
+        newAlt: 'Baymax'
+    },
+    {
+        id: 4,
+        image: 'bumblebee.webp',
+        newAlt: 'Bumblebee'
+    },
+    {
+        id: 5,
+        image: 'clank.webp',
+        newAlt: 'Clank'
+    },
+    {
+        id: 6,
+        image: 'gipsy-danger.webp',
+        newAlt: 'Gipsy-danger'
+    },
+    {
+        id: 7,
+        image: 'r2d2.webp',
+        newAlt: 'R2d2'
+    },
+    {
+        id: 8,
+        image: 'rodney.webp',
+        newAlt: 'Rodney'
+    },
+    {
+        id: 9,
+        image: 'wall-e.webp',
+        newAlt: 'Wall-e'
+    },
+    {
+        id: 10,
+        image: 'wild-robot.webp',
+        newAlt: 'Wild-robot'
+    },
+    {
+        id: 11,
+        image: 'astro.webp',
+        newAlt: 'Astro'
+    },
+    {
+        id: 12,
+        image: 'atom.webp',
+        newAlt: 'Atom'
+    },
+    {
+        id: 13,
+        image: 'baymax.webp',
+        newAlt: 'Baymax'
+    },
+    {
+        id: 14,
+        image: 'bumblebee.webp',
+        newAlt: 'Bumblebee'
+    },
+    {
+        id: 15,
+        image: 'clank.webp',
+        newAlt: 'Clank'
+    },
+    {
+        id: 16,
+        image: 'gipsy-danger.webp',
+        newAlt: 'Gipsy-danger'
+    },
+    {
+        id: 17,
+        image: 'r2d2.webp',
+        newAlt: 'R2d2'
+    },
+    {
+        id: 18,
+        image: 'rodney.webp',
+        newAlt: 'Rodney'
+    },
+    {
+        id: 19,
+        image: 'wall-e.webp',
+        newAlt: 'Wall-e'
+    },
+    {
+        id: 20,
+        image: 'wild-robot.webp',
+        newAlt: 'Wild-robot'
+    },
+]
 
-  function flipCard() {
-    if (lockBoard) return;
-    if (this === firstCard) return;
+// function to restart the game
+const restartGame = () => {
+    let toggledCard = 
+        document.getElementsByClassName('card toggled');
+    imagesLinkArray.sort(() => Math.random() - 0.5);
+    Object.values(toggledCard).forEach(function (el) {
+        setTimeout(() => {
+            el.classList.remove("toggled");
+        }, 0);
+    })
+    toggledCardsArray.length = 0;
+    move = 0;
+    movesDisplay.innerText = `Moves: ${move}`;
+    let allImagesSrc = document.getElementsByClassName('front-face');
+    Object.values(allImagesSrc).forEach((el, index)=>{
+        el.src = imagesLinkArray[index].image;
+        el.alt = imagesLinkArray[index].newAlt;
+        el.id = imagesLinkArray[index].id
+    }) 
+}
+restart.addEventListener('click', restartGame);
 
-    this.classList.add('flip');
-
-    if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
-      return;
-    }
-
-    secondCard = this;
-    lockBoard = true;
-
-    checkForMatch();
-  }
-
-  function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-    isMatch ? disableCards() : unflipCards();
-  }
-
-  function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-
-    resetBoard();
-  }
-
-  function unflipCards() {
-    setTimeout(() => {
-      firstCard.classList.remove('flip');
-      secondCard.classList.remove('flip');
-
-      resetBoard();
-    }, 1500);
-  }
-
-  function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-  }
-
- (function shuffle() {
-   cards.forEach(card => {
-     let ramdomPos = Math.floor(Math.random() * 12);
-     card.style.order = ramdomPos;
-   });
- })();
-
-  cards.forEach(card => card.addEventListener('click', flipCard));
+//checking for the last clicked and current 
+//clicked cards and applying changes accordingly
+for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function () {
+        this.classList.add("toggled");
+        toggledCardsArray.push(this);
+        let thisImgSrc = this.querySelector('.front-face').src;
+        let previousImgSrc = 
+        toggledCardsArray[toggledCardsArray.length - 2].querySelector('.front-face').src;
+        if(thisImgSrc !== previousImgSrc) {
+            toggledCardsArray.forEach(function (el) {
+                setTimeout(() => {
+                    el.classList.remove("toggled");
+                }, 500);
+            })
+            toggledCardsArray.length = 0;
+            move++;
+        }
+        else{
+            toggledCardsArray.length = 0;
+            move++;
+        }
+        movesDisplay.innerText = `Moves: ${move}`;
+    })
+}
